0c4079e6961c928ccf89cb6d090f35b97c1b94c9
floccFlocc <andrei.palma2000@gmail.com>
2023-10-19 14:14:42 +0800

    Added condition for students

--- Simple_Grade-Checker/Program.cs
+++ Simple_Grade-Checker/Program.cs
@@ -1,21 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
+
 string userName = "";
 string passWord = "";
 string admin = "teacher";
 string adminPassWord = "1234";
+string student = "student";
+string studentPassword = "1234";
 string option = "";
-string name = "";
+string firstname = "";
+string lastname = "";
 
 Console.WriteLine("Hello! This is my first C# console project.\n\n");
 
 Console.WriteLine("Please login to continue.");
 
-Console.WriteLine("Username:");
-userName = Console.ReadLine();
-
-Console.WriteLine("Password:");
-passWord = Console.ReadLine();
+login();
 
 Console.Clear();
 
@@ -27,12 +28,40 @@ if (userName.ToLower() == admin && passWord.ToLower() == adminPassWord) {
     Console.WriteLine("(2) Remove A Studet");
     Console.WriteLine("(3) Add A Student");
     Console.WriteLine("(4) Compute A Student Grade");
+    Console.WriteLine("(5) Logout");
 
     option = Console.ReadLine();
 
 }
-else {
+else if (userName.ToLower() == student && passWord.ToLower() == studentPassword) {
 
-    Console.WriteLine("Please provide your full name to view your grades");
-    name = Console.ReadLine();
+    Console.WriteLine("Please provide your fill out this to view your grades.");
+    Console.Write("First Name: ");
+    firstname = Console.ReadLine();
+    Console.Write("Last Name: ");
+    lastname = Console.ReadLine();
+
+    showGrade(firstname);
+
+}
+else
+{
+    Console.WriteLine("Access Denied. Please Try Again.");
+    login();
+}
+
+
+void login()
+{
+    Console.Write("Username:");
+    userName = Console.ReadLine();
+
+    Console.Write("Password:");
+    passWord = Console.ReadLine();
+}
+
+void showGrade(string name)
+{
+    Console.Clear();
+    Console.WriteLine("Hi! " + name + ".");
 }
